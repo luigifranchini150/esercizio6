e499ffd59c35a584b135b04abc2ab39a2c1090fd
luigifranchini150 <luigifranchini150@gmail.com>
2025-06-05 13:02:11 +0200

    Add files via upload

---
+++ selfwork operatore 2/selfwork operatore 2.js
@@ -0,0 +1,13 @@
+let annoCorrente = 2025;
+let annoDiNascita = 1967;
+
+let age = annoCorrente - annoDiNascita
+console.log(age);
+
+let anniMancanti = 100 - age
+console.log(anniMancanti);
+
+console.log(`hai ${age} anni che ti mancano ${anniMancanti} anni per arrivare a 100`);
+
+
+

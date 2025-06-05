69ca74b822378016dc161606dc321bec00bda86b
luigifranchini150 <luigifranchini150@gmail.com>
2025-06-05 11:25:20 +0200

    Add files via upload

---
+++ selfwork operatore 1/selfwork operatore 1.js
@@ -0,0 +1,12 @@
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+let num4 = 4;
+let num5 = 5;
+
+let somma = num1 + num2 + num3 + num4 + num5
+console.log(somma);
+
+let media = somma / 5
+console.log(media);
+console.log(`la somma tra i numeri equivale a ${somma}, la media equivale a ${media}`)

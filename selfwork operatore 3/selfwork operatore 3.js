f4106713964aa60cd253f419afe9440a6edbe450
luigifranchini150 <luigifranchini150@gmail.com>
2025-06-06 14:01:38 +0200

    Add files via upload

---
+++ selfwork operatore 3/selfwork operatore 3.js
@@ -0,0 +1,21 @@
+let num1 = parseInt (prompt(`inserisci il primo numero`))
+let num2 = parseInt (prompt(`inserisci il secondo numero`))
+let somma = num1 + num2
+let sottrazione = num1 - num2
+let moltiplicazione = num1 * num2
+let divisione = num1 / num2
+let potenza = num1 ** num2
+
+
+console.log(`con i numeri da te sceti, i risultati delle operazioni sono:`);
+
+console.log(`somma = ${somma}, sottrazione${sottrazione}, moltiplicaziione${moltiplicazione}, divisiuone${divisione}, potenza${potenza}`);
+
+
+
+
+
+
+
+
+
